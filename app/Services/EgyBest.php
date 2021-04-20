cc674e77dff50bfb7b96b43d75933079679103c8
unknown <elsayedkamal581999@gmail.com>
2021-04-20 20:28:12 +0200

    “wip”

--- app/Services/EgyBest.php
+++ app/Services/EgyBest.php
@@ -32,7 +32,7 @@ class EgyBest
     {
         $crawler = goutte::request('GET', self::$baseUrl . "/movie/{$slug}");
         $error = $crawler->filter('.msg_box.error')->text('no_error');
-        return $crawler->html();
+        dd($crawler);
         if ($error != "no_error") {
             return $error;
         }
