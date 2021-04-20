a0e6793ec6ee116d557fd564b15aafe9b3aa23b2
unknown <elsayedkamal581999@gmail.com>
2021-04-20 20:17:53 +0200

    “wip”

--- routes/api.php
+++ routes/api.php
@@ -6,20 +6,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('egybest/search/{title}', function ($title) {
-    try {
-        $data = EgyBest::search($title, request('page'));
-    } catch (\Throwable $th) {
-        //return response()->json(['error' => $th->getMessage()], 500);
-    }
+    // try {
+    //     $data = EgyBest::search($title, request('page'));
+    // } catch (\Throwable $th) {
+    //     return response()->json(['error' => $th->getMessage()], 500);
+    // }
+    $data = EgyBest::search($title, request('page'));
     return response()->json(['data' => $data]);
 });
 
 Route::get('egybest/movie/{slug}', function ($slug) {
-    try {
-        $data = EgyBest::show($slug);
-    } catch (\Throwable $th) {
-        //return response()->json(['error' => $th->getMessage()], 500);
-    }
+    // try {
+    //     $data = EgyBest::show($slug);
+    // } catch (\Throwable $th) {
+    //     return response()->json(['error' => $th->getMessage()], 500);
+    // }
+    $data = EgyBest::show($slug);
 
     if (is_string($data)) {
         return response()->json(['error' => $th->getMessage()] , 404);
